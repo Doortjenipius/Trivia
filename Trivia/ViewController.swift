ee269c800ccee61e7e4849862f7de8bdadc260b9
doortje <doortje@wcw-stud-145-109-29-104.wireless.uva.nl>
2018-12-10 10:33:30 +0100

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by doortje on 10/12/2018.
+//  Copyright © 2018 Doortje. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
